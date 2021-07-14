15df8177c3f7ae3d432dd6b8dca170a676eeb132
shefinjoy <87313559+shefinjoy@users.noreply.github.com>
2021-07-14 19:30:48 +0530

    Add files via upload

---
+++ Fibonacci series.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def Fibonacci(Number):
+    if(Number<0):
+        print("enter whole number")
+
+    elif(Number == 0):
+        return 0
+    elif(Number == 1):
+        return 1
+    else:
+        return (Fibonacci(Number - 2) + Fibonacci(Number - 1))
+
+n = int(input("Enter the value of 'n': "))
+print("Fibonacci Series:", end = ' ')
+for n in range(0, n):
+  print(Fibonacci(n), end = ' ')
+        
