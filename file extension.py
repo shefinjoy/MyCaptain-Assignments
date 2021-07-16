9a99e8a1cad5f2c0ec8aabde223790d1641bbb79
shefinjoy <87313559+shefinjoy@users.noreply.github.com>
2021-07-16 19:17:30 +0530

    Add files via upload

---
+++ file extension.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 16 10:15:21 2021
+
+@author: shefi
+"""
+
+fname = input("filename is:")
+ext = fname.split(".")
+print ("extension is:" + repr(ext[-1]))
